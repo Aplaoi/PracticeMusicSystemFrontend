b09cb75509e1bb29f2669eddd35bb21b7ec4613c
apl aoi <18350009019@163.com>
2024-11-20 19:04:58 +0800

    进一步完善

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,15 @@
 	import {ElMessage} from "element-plus";
 
 	const formData=ref(null);
-	const isOpen=ref(false);
+	const isNewOpen=ref(false);
+	const isModifyOpen=ref(false);
 
 	const name=ref('');
 	const singer=ref('');
+
+	const modifiedName=ref('');
+	const modifiedSinger=ref('');
+	const modifiedID=ref(0);
 
 	//删除music
 	function deleteData(id){
@@ -17,26 +22,26 @@
 				message:"已删除",
 				type:'success'
 			})
-			getData()
+			getAllData()
 		}
 		).catch(()=>console.log("删除失败"))
 	}
 
 	//获取歌曲列表
-	function getData(){
+	function getAllData(){
 		axios.get("http://localhost:8080/musics").then(
 			response=>formData.value=response.data.data
 		).catch(()=>console.log("失败"))
 	}
 
 	//添加新歌曲
-	function NewData(postForm){
+	function newData(postForm){
 		postForm.name=name.value;
 		postForm.singer=singer.value;
 		axios.post(`http://localhost:8080/musics`,postForm).then(()=>{
 				console.log("成功添加");
-				getData();
-				isOpen.value=false;
+				getAllData();
+				isNewOpen.value=false;
 				ElMessage({
 					message:"添加成功",
 					type:'success'
@@ -47,12 +52,33 @@
 		).catch((error)=>console.log("添加失败"+error))
 	}
 
-	//修改现有歌曲
-	function modifyData(){
-		axios.put("http://localhost:8080/musics")
+	//获取对应id的信息
+	function getDataById(id){
+		axios.get(`http://localhost:8080/musics/${id}`).then((response)=>{
+			console.log(response);
+			modifiedName.value=response.data.data.name;
+			modifiedSinger.value=response.data.data.singer;
+		}).catch((err)=>console.log("失败"+err))
 	}
 
-	onMounted(()=>getData());
+	//修改现有歌曲
+	function modifyData(id){
+		let putForm={
+			id:id,
+			name:modifiedName.value,
+			singer:modifiedSinger.value
+		}
+		axios.put("http://localhost:8080/musics",putForm).then(()=>{
+			ElMessage({
+				message:"修改成功",
+				type:"success"
+			});
+			getAllData();
+			isModifyOpen.value=false;
+		}).catch(()=>ElMessage({message:"失败",type:"error"}))
+	}
+
+	onMounted(()=>getAllData());
 
 </script>
 
@@ -80,12 +106,11 @@
 					<span>Navigator Two</span>
 				</el-menu-item>
 				<el-menu-item index="4">
-
 					<span>Navigator Four</span>
 				</el-menu-item>
 			</el-menu>
 
-	<div class="button"><el-button type="primary" @click="isOpen=true">新增音乐</el-button></div>
+	<div class="button"><el-button type="primary" @click="isNewOpen=true">新增音乐</el-button></div>
 	<br>
 	<div class="table">
 		<el-table :data="formData" border stripe style="width: 800px">
@@ -98,15 +123,18 @@
 			<el-table-column align="center" prop="singer" label="Singer" width="250"/>
 			<el-table-column align="center" label="Operations" width="200">
 				<template #default="scope">
-					<el-button type="primary" size="small" @click="isOpen=true">编辑</el-button>
+					<el-button type="primary" size="small"
+							   @click="isModifyOpen=true;
+							   modifiedID=scope.row.id;
+								getDataById(modifiedID)">编辑</el-button>
 					<el-button type="danger" size="small" @click="deleteData(scope.row.id)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
 	</div>
 
-	<!--修改弹窗-->
-	<el-dialog v-model="isOpen" title="添加" width="500">
+	<!--添加弹窗-->
+	<el-dialog v-model="isNewOpen" title="添加" width="500">
 		<el-form>
 			<el-form-item label="Name">
 				<el-input v-model="name"></el-input>
@@ -119,13 +147,37 @@
 		</el-form>
 		<template #footer>
 			<div class="dialog-footer">
-				<el-button @click="isOpen = false">取消</el-button>
-				<el-button type="primary" @click="NewData">
+				<el-button @click="isNewOpen = false">取消</el-button>
+				<el-button type="primary" @click="newData">
 					保存
 				</el-button>
 			</div>
 		</template>
 	</el-dialog>
+
+	<!--	修改弹窗-->
+	<el-dialog v-model="isModifyOpen" title="修改" width="500">
+		<el-form>
+			<el-form-item label="Name">
+				<el-input v-model="modifiedName"></el-input>
+			</el-form-item>
+		</el-form>
+		<el-form>
+			<el-form-item label="Singer">
+				<el-input v-model="modifiedSinger"></el-input>
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<div class="dialog-footer">
+				<el-button @click="isModifyOpen = false">取消</el-button>
+				<el-button type="primary" @click="modifyData(modifiedID)">
+					保存
+				</el-button>
+			</div>
+		</template>
+	</el-dialog>
+
+
 <!--	<el-pagination background layout="prev, pager, next" :total="100" />-->
 	<el-backtop :right="100" :bottom="100" />
 </template>
